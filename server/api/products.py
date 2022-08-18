b7e9c896d2df6a39a9e367dd2e74d6cf4a00642b
sergiobarria <sbarria.dev@gmail.com>
2022-08-17 22:50:17 -0500

    feat(server): create, read routes

---
+++ server/api/products.py
@@ -0,0 +1,16 @@
+products = [
+    {
+        "id": 1,
+        "name": "The Forest Hiker",
+        "duration": 5,
+        "max_group_size": 25,
+        "difficulty": "easy",
+        "ratings_average": 4.7,
+        "ratings_quantity": 37,
+        "price": 397,
+        "summary": "Breathtaking hike through the Canadian Banff National Park",
+        "description": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "cover_image": "/img/tour-1-cover.jpg",
+        "start_dates": "2021-04-25,10:00",
+    }
+]
--- server/api/routes.py
+++ server/api/routes.py
@@ -1,8 +1,40 @@
 from ninja import NinjaAPI
 
+from api.schemas import NotFoundSchema
+
+from .products import products
+
 app = NinjaAPI()
 
 
 @app.get("/healthcheck")
 def api_healthcheck(request):
+    """Check if the API is up and running."""
     return {"status": "ok"}
+
+
+@app.get("/products")
+def get_all_products(request):
+    """Return a list of all products"""
+    return {"data": products}
+
+
+@app.get("/products/{product_id}")
+def get_product_by_id(request, product_id: int):
+    """Return a single product by ID"""
+    product = None
+    for p in products:
+        if p["id"] == product_id:
+            product = p
+            break
+
+    # if not product:
+    #     return 404, {"message": "Product not found"}
+
+    return {"data": product}
+    # try:
+    #     for p in products:
+    #         if p["id"] == product_id:
+    #             return {"data": p}
+    # except:
+    #     return 404, {"message": "Product not found"}
